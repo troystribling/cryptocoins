da030763ed7d8396141792fa48e475726f8481a2
Troy Stribling <troy.stribling@gmail.com>
2017-12-01 22:31:36 -0800

    top pairs model works

--- cryptocoins/models/currency_pairs_history.py
+++ cryptocoins/models/currency_pairs_history.py
@@ -15,7 +15,7 @@ class CurrencyPairsHistory(BaseModel):
     created_at = DateTimeField()
     from_symbol = TextField(index=True)
     to_symbol = TextField(index=True)
-    exchanges = TextField()
+    exchange = TextField()
     volume_from_24_hour = DecimalField()
     volume_to_24_hour = DecimalField()
 
@@ -27,19 +27,18 @@ class CurrencyPairsHistory(BaseModel):
 
     @classmethod
     def create_from_top_pairs(cls, top_pairs, batch_size=100):
-        print(top_pairs)
         with database.atomic():
             for i in range(0, len(top_pairs), batch_size):
-                model_params = [cls.top_pair_to_model_params(top_pair) for top_pair in top_pairs[i:i + batch_size]]
-                print(model_params)
+                model_params = [cls.top_pairs_to_model_params(top_pair) for top_pair in top_pairs[i:i + batch_size]]
                 cls.insert_many(model_params).execute()
 
     @classmethod
     def top_pairs_to_model_params(cls, top_pair):
-        expected_keys = ['exchange', 'fromSymbol', 'toSymbol', 'volum24h', 'volume24hTo']
+        expected_keys = ['exchange', 'fromSymbol', 'toSymbol', 'volume24h', 'volume24hTo']
         if not valid_params(expected_params=expected_keys, params=top_pair):
             raise ValueError('ERROR: Top Pairs keys invalid')
         return {'from_symbol': top_pair['fromSymbol'],
                 'to_symbol': top_pair['toSymbol'],
-                'volume_from_24_hour': top_pair['volum24h'],
+                'exchange': top_pair['exchange'],
+                'volume_from_24_hour': top_pair['volume24h'],
                 'volume_to_24_hour': top_pair['volume24hTo']}
--- notebooks/import_data.py
+++ notebooks/import_data.py
@@ -11,12 +11,14 @@ from cryptocoins.models.coins_price_history import CoinsPriceHistory
 
 
 bucket_name = 'gly.fish'
-start_date = parse('20171125')
-end_date = parse('20171125')
 
 
 # %%
 # coin_list
+start_date = parse('20171125')
+end_date = parse('20171125')
+
+
 @import_from_s3(bucket_name=bucket_name, start_date=start_date, end_date=end_date, remote_dir='cryptocoins/cryptocompare/coin_list')
 def import_coin_list(data):
     for coin in data[0]['Data'].values():
@@ -28,6 +30,10 @@ import_coin_list()
 
 # %%
 # coin_snapshot
+start_date = parse('20171125')
+end_date = parse('20171125')
+
+
 @import_from_s3(bucket_name=bucket_name, start_date=start_date, end_date=end_date, remote_dir='cryptocoins/cryptocompare/coin_snapshot')
 def import_coin_snapshot(data):
     coin_snapshot = data[0]['Data']
@@ -40,10 +46,13 @@ import_coin_snapshot()
 
 # %%
 # top_currency_pairs
+start_date = parse('20171126')
+end_date = parse('20171126')
+
+
 @import_from_s3(bucket_name=bucket_name, start_date=start_date, end_date=end_date, remote_dir='cryptocoins/cryptocompare/top_pairs')
 def import_currency_pairs_history(data):
     top_pairs = data[0]['Data']
-    print(top_pairs)
     CurrencyPairsHistory.create_from_top_pairs(top_pairs)
 
 
@@ -52,6 +61,10 @@ import_currency_pairs_history()
 
 # %%
 # coin_price_history
+start_date = parse('20171125')
+end_date = parse('20171125')
+
+
 @import_from_s3(bucket_name=bucket_name, start_date=start_date, end_date=end_date, remote_dir='cryptocoins/cryptocompare/histoday')
 def import_coin_price_history(data):
     histoday = data[0]['Data']
