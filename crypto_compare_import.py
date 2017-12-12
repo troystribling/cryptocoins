3a328883a5bd34b6235df07f05bdff4e3e2e607a
Troy Stribling <troy.stribling@gmail.com>
2017-12-12 08:43:28 -0800

    fixed import bug

--- crypto_compare_import.py
+++ crypto_compare_import.py
@@ -7,15 +7,13 @@ from cryptocoins.crypto_compare.imports import import_coin_list
 from cryptocoins.crypto_compare.imports import import_currency_pairs_history
 from cryptocoins.crypto_compare.imports import import_coin_price_history
 
-print(f"IMPORTING {start_date} TO {end_date} FROM {bucket_name}")
-
 start_date = parse(sys.argv[1]) if len(sys.argv) > 1 else date.today()
 end_date = parse(sys.argv[2]) if len(sys.argv) > 2 else start_date
 
-print(f"IMPORTING {start_date} TO {end_date} FROM {bucket_name}")
+print(f"IMPORTING {start_date} TO {end_date}")
 
 if __name__ == "__main__":
-    import_coin_snapshot(start_date, end_date)
     import_coin_list(start_date, end_date)
+    import_coin_snapshot(start_date, end_date)
     import_currency_pairs_history(start_date, end_date)
     import_coin_price_history(start_date, end_date)
