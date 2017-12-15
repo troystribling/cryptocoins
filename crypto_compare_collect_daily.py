1145cbd51d465b6d5aa4bdccdbfea4c2f05a43ae
Troy Stribling <troy.stribling@gmail.com>
2017-12-15 08:51:04 -0800

    verifying tiestamp updates

--- crypto_compare_collect_daily.py
+++ crypto_compare_collect_daily.py
@@ -5,21 +5,19 @@ from datetime import datetime
 from cryptocoins.crypto_compare.requests import request_coin_list
 from cryptocoins.crypto_compare.requests import request_top_currency_pairs
 from cryptocoins.crypto_compare.requests import request_coin_snapshot
-from cryptocoins.crypto_compare.requests import request_coin_price_history
 
 from cryptocoins.crypto_compare.imports import import_coin_snapshot
 from cryptocoins.crypto_compare.imports import import_coin_list
 from cryptocoins.crypto_compare.imports import import_currency_pairs_history
-from cryptocoins.crypto_compare.imports import import_coin_price_history
 
 from cryptocoins.models.coins import Coins
 from cryptocoins.models.currency_pairs_history import CurrencyPairsHistory
 from cryptocoins.models.exchanges_history import ExchangesHistory
 
 
-max_coins = 200
-max_pairs = 100
-max_exchanges = 100
+max_coins = 5
+max_pairs = 5
+max_exchanges = 5
 
 # coins
 start_date = datetime.utcnow()
@@ -64,22 +62,3 @@ end_date = datetime.utcnow()
 print(f"COMPLETED coins_history and exchanges_history {end_date}")
 
 import_coin_snapshot(start_date, end_date)
-
-# collect history
-start_date = datetime.utcnow()
-print(f"INITIALIZE histoday {start_date}")
-
-for coin in Coins.top_coins(limit=max_coins):
-    for currency_pair in CurrencyPairsHistory.currency_pairs_for_coin(coin.symbol, limit=max_pairs):
-        print(f"REQUESTING histoday for CCCAGG, {currency_pair.from_symbol}, {currency_pair.to_symbol}")
-        request_coin_price_history(currency_pair.from_symbol, currency_pair.to_symbol, allData='true')
-        sleep(2.0)
-        for exchange in ExchangesHistory.top_exchanges_for_currency_pair(currency_pair.from_symbol, currency_pair.to_symbol, limit=max_exchanges):
-            print(f"REQUESTING histoday for {exchange.name}, {exchange.from_symbol}, {exchange.to_symbol}")
-            request_coin_price_history(exchange.from_symbol, exchange.to_symbol, exchange=exchange.name, allData='true')
-            sleep(2.0)
-
-end_date = datetime.utcnow()
-print(f"COMPLETED histoday {end_date}")
-
-import_coin_price_history(start_date, end_date)
---
+++ crypto_compare_collect_histoday.py
@@ -0,0 +1,32 @@
+from time import sleep
+from datetime import datetime
+
+from cryptocoins.crypto_compare.requests import request_coin_price_history
+from cryptocoins.crypto_compare.imports import import_coin_price_history
+
+from cryptocoins.models.coins import Coins
+from cryptocoins.models.currency_pairs_history import CurrencyPairsHistory
+from cryptocoins.models.exchanges_history import ExchangesHistory
+
+max_coins = 200
+max_pairs = 100
+max_exchanges = 100
+
+# collect history
+start_date = datetime.utcnow()
+print(f"INITIALIZE histoday {start_date}")
+
+for coin in Coins.top_coins(limit=max_coins):
+    for currency_pair in CurrencyPairsHistory.currency_pairs_for_coin(coin.symbol, limit=max_pairs):
+        print(f"REQUESTING histoday for CCCAGG, {currency_pair.from_symbol}, {currency_pair.to_symbol}")
+        request_coin_price_history(currency_pair.from_symbol, currency_pair.to_symbol, allData='true')
+        sleep(2.0)
+        for exchange in ExchangesHistory.top_exchanges_for_currency_pair(currency_pair.from_symbol, currency_pair.to_symbol, limit=max_exchanges):
+            print(f"REQUESTING histoday for {exchange.name}, {exchange.from_symbol}, {exchange.to_symbol}")
+            request_coin_price_history(exchange.from_symbol, exchange.to_symbol, exchange=exchange.name, allData='true')
+            sleep(2.0)
+
+end_date = datetime.utcnow()
+print(f"COMPLETED histoday {end_date}")
+
+import_coin_price_history(start_date, end_date)
