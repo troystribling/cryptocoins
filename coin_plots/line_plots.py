024d3fb9dbaea9da8f8a81b83420dff0947e519e
Troy Stribling <troy.stribling@gmail.com>
2017-12-29 19:13:00 -0600

    staring jupyter notebook

---
+++ coin_plots/line_plots.py
@@ -0,0 +1,3 @@
+import numpy
+import matplotlib.pyplot as pyplot
+import seaborn
--- cryptocoins/utils.py
+++ cryptocoins/utils.py
@@ -28,13 +28,13 @@ def valid_params(expected_params, params):
 def null_param_if_missing(expected_params, params):
     for expected_param in expected_params:
         if expected_param not in params:
-            logger.error(f"'{expected_param}' KEY IS MISSING FROM {params}")
+            logger.warn(f"'{expected_param}' KEY IS MISSING FROM {params}")
             params[expected_param] = None
 
 def setup_logging(file_name=None, max_bytes=200000000, backup_count=5):
     logger = logging.getLogger("cryptocoins")
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('(%(levelname)s|%(asctime)s|%(module)s|%(filename)s) %(message)s')
+    formatter = logging.Formatter('(%(levelname)s|%(asctime)s|%(module)s|%(filename)s, %(lineno)s) %(message)s')
     if file_name is not None:
         handler = RotatingFileHandler(file_name, "a", max_bytes, backup_count)
         handler.setFormatter(formatter)
