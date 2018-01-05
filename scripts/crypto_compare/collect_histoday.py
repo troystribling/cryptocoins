b8a5cd28d1cc81a7789f1941c1084001d093f6d1
Troy Stribling <troy.stribling@gmail.com>
2018-01-05 13:35:38 -0800

    Update collect_histoday.py

--- scripts/crypto_compare/collect_histoday.py
+++ scripts/crypto_compare/collect_histoday.py
@@ -22,7 +22,7 @@ max_coins = sys.argv[2] if len(sys.argv) > 2 else 300
 max_pairs = sys.argv[3] if len(sys.argv) > 3 else 300
 max_exchanges = sys.argv[4] if len(sys.argv) > 4 else 100
 
-log_out = sys.argv[5] if len(sys.argv) > 5 else '/var/log/apps/cryptocoins/crypto_compare_daily.log'
+log_out = sys.argv[5] if len(sys.argv) > 5 else '/var/log/apps/cryptocoins/crypto_compare_histoday.log'
 
 if log_out == 'stdout':
     logger = setup_logging()
