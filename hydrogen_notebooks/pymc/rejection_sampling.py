72e2e886511c206cb8bdc7a367acfec66b1490e5
Troy Stribling <troy.stribling@gmail.com>
2018-02-19 13:00:15 -0800

    added inverse cd sampling example

--- hydrogen_notebooks/pymc/rejection_sampling.py
+++ hydrogen_notebooks/pymc/rejection_sampling.py
@@ -45,7 +45,7 @@ sympy.plot(cdf, (x, 0, 6), ylabel="CDF(x)", xlabel='x', ylim=(0, 1))
 x = sympy.symbols('x')
 intervals = [(1, sympy.Interval(0, 1 / 6, False, True).contains(x)), (2, sympy.Interval(1 / 6, 2 / 6, False, True).contains(x)), (3, sympy.Interval(2 / 6, 3 / 6, False, True).contains(x)), (4, sympy.Interval(3 / 6, 4 / 6, False, True).contains(x)), (5, sympy.Interval(4 / 6, 5 / 6, False, True).contains(x)), (6, sympy.Interval(5 / 6, 1, False, False).contains(x))]
 inv_cdf = sympy.Piecewise(*intervals)
-samples = [int(inv_cdf.subs(x, i)) for i in numpy.random.rand(1000)]
+samples = [int(inv_cdf.subs(x, i)) for i in numpy.random.rand(10000)]
 n, bins, _ = pyplot.hist(samples, bins=[1, 2, 3, 4, 5, 6, 7], density=True, align='left', rwidth=0.9)
 pyplot.title("Simulated PDF")
 
@@ -70,4 +70,17 @@ axis.bar(random_variable_values, cdf_values, 0.4, color="#348ABD", label=f"CDF",
 axis.legend()
 
 # %%
-# For continuous distribution
+# For the continuous distribution exp(x)
+
+f_inv = lambda v: numpy.log(1.0 / (1.0 - v))
+samples = [f_inv(v) for v in numpy.random.rand(10000)]
+
+figure, axis = pyplot.subplots(figsize=(12, 5))
+axis.set_xlabel("Sample")
+axis.set_ylabel("PDF")
+axis.set_title("Inverse Sampling")
+axis.grid(True, zorder=5)
+_, bins, _ = axis.hist(samples, 50, density=True, color="#348ABD", alpha=0.6, label=f"Sampled Density", edgecolor="#348ABD", lw="3", zorder=10)
+sample_values = [numpy.exp(-v) for v in bins]
+axis.plot(bins, sample_values, color="#A60628", label=f"Sampled Function", lw="3", zorder=10)
+axis.legend()
