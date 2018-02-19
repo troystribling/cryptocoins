922c56b66bf1b78ea680f5110f437ac15635ab48
Troy Stribling <troy.stribling@gmail.com>
2018-02-18 17:37:11 -0800

    more work on rejection sampling

--- hydrogen_notebooks/pymc/rejection_sampling.py
+++ hydrogen_notebooks/pymc/rejection_sampling.py
@@ -24,9 +24,6 @@ def linear(x):
         return None
     return 2.0 * x
 
-def samples_plot(cdf, x):
-    figure, axis = pyplot
-
 # %%
 # Inverse CDF Descrete random variables
 
@@ -46,6 +43,31 @@ sympy.plot(cdf, (x, 0, 6), ylabel="CDF(x)", xlabel='x', ylim=(0, 1))
 # %%
 # The inverse of the heavyside distribution is given by
 x = sympy.symbols('x')
-intervals = [(0, x < 0), (1, sympy.Interval(0, 1 / 6).contains(x)), (2, sympy.Interval(1 / 6, 2 / 6).contains(x)), (3, sympy.Interval(2 / 6, 3 / 6).contains(x)), (4, sympy.Interval(3 / 6, 4 / 6).contains(x)), (4, sympy.Interval(4 / 6, 5 / 6).contains(x)), (6, x > 5 / 6)]
+intervals = [(1, sympy.Interval(0, 1 / 6, False, True).contains(x)), (2, sympy.Interval(1 / 6, 2 / 6, False, True).contains(x)), (3, sympy.Interval(2 / 6, 3 / 6, False, True).contains(x)), (4, sympy.Interval(3 / 6, 4 / 6, False, True).contains(x)), (5, sympy.Interval(4 / 6, 5 / 6, False, True).contains(x)), (6, sympy.Interval(5 / 6, 1, False, False).contains(x))]
 inv_cdf = sympy.Piecewise(*intervals)
-samples = [inv_cdf.subs(x, i) for i in rand(10)]
+samples = [int(inv_cdf.subs(x, i)) for i in numpy.random.rand(1000)]
+n, bins, _ = pyplot.hist(samples, bins=[1, 2, 3, 4, 5, 6, 7], density=True, align='left', rwidth=0.9)
+pyplot.title("Simulated PDF")
+
+# %%
+cdf_value = 0.0
+sampled_cdf = numpy.zeros(len(n))
+for i in range(0, len(n)):
+    cdf_value += n[i]
+    sampled_cdf[i] = cdf_value
+
+cdf_values = [float(cdf.subs(x, i)) for i in range(0, 6)]
+
+figure, axis = pyplot.subplots(figsize=(12, 5))
+axis.set_xlabel("Sample")
+axis.set_ylabel("Value")
+axis.set_title("Simulated CDF")
+axis.grid(True, zorder=5)
+random_variable_values = [i + 0.2 for i in range(1, 7)]
+axis.bar(random_variable_values, sampled_cdf, 0.4, color="#A60628", label=f"Sampled CDF Estimate", alpha=0.6, lw="3", edgecolor="#A60628", zorder=10)
+random_variable_values = [i - 0.2 for i in range(1, 7)]
+axis.bar(random_variable_values, cdf_values, 0.4, color="#348ABD", label=f"CDF", alpha=0.6, lw="3", edgecolor="#348ABD", zorder=10)
+axis.legend()
+
+# %%
+# For continuous distribution
